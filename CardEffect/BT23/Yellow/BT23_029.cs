e6898c55a6d8eaf1d513f40bab8e912e153eb3b9
x89rt2 <x89rt2@gmail.com>
2025-12-07 22:38:52 -0500

    Update BT23_029.cs
    Removed the requirement for it to not be this card. Not sure about the null on line 63 and wondering why we have 2 methods to checking traits.

--- CardEffect/BT23/Yellow/BT23_029.cs
+++ CardEffect/BT23/Yellow/BT23_029.cs
@@ -71,8 +71,7 @@ namespace DCGO.CardEffects.BT23
                 bool PermanentCondition(Permanent permanent)
                 {
                     return CardEffectCommons.IsPermanentExistsOnOwnerBattleAreaDigimon(permanent, card)
-                        && (permanent.TopCard.EqualsTraits("Beast") || permanent.TopCard.EqualsTraits("Beastkin") || permanent.TopCard.HasCSTraits)
-                        && permanent != card.PermanentOfThisCard();
+                        && (permanent.TopCard.EqualsTraits("Beast") || permanent.TopCard.EqualsTraits("Beastkin") || permanent.TopCard.HasCSTraits);
                 }
 
                 bool IsOpponentsDigimon(Permanent permanent)
