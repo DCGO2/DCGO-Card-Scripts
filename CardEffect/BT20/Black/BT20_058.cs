aec2884da57f5cf33c76837e296ba694a5f49530
x89rt2 <x89rt2@gmail.com>
2025-12-15 18:21:09 -0500

    Update BT20_058.cs
    Fixed Misspelling

--- CardEffect/BT20/Black/BT20_058.cs
+++ CardEffect/BT20/Black/BT20_058.cs
@@ -129,7 +129,7 @@ namespace DCGO.CardEffects.BT20
 
                 string EffectDiscription()
                 {
-                    return "[All Turns] When this Digimon would leave the battle are, you may play 1 play cost 11 or lower Digimon card with the [Machine] or [Cyborg] trait from this Digimon's digivolution cards without paying the cost.";
+                    return "[All Turns] When this Digimon would leave the battle area, you may play 1 play cost 11 or lower Digimon card with the [Machine] or [Cyborg] trait from this Digimon's digivolution cards without paying the cost.";
                 }
 
                 bool CanSelectCardCondition(CardSource cardSource)
