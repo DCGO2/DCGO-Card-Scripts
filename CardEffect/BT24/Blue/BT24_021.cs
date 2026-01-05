6bfaa47114a2bcb735e99af3dbfec98d1b594632
Kagemaru656 <hooperk@tcd.ie>
2026-01-05 19:17:12 +0000

    Fix Inherited Effect

--- CardEffect/BT24/Blue/BT24_021.cs
+++ CardEffect/BT24/Blue/BT24_021.cs
@@ -142,21 +142,25 @@ namespace DCGO.CardEffects.BT24
                 {
                     return cardSource.IsDigimon && 
                         (cardSource.EqualsCardName("Titamon") || cardSource.EqualsTraits("Demon")) && 
-                        cardSource.CanPlayCardTargetFrame(card.PermanentOfThisCard().PermanentFrame, true, activateClass);
+                        cardSource.CanPlayCardTargetFrame(card.PermanentOfThisCard().PermanentFrame, 
+                                                            true, 
+                                                            activateClass, 
+                                                            fixedCost: Math.max(0, cardSource.CostList(card.PermanentOfThisCard(), false, true).Min() -1));
                 }
 
                 bool CanUseCondition(Hashtable hashtable)
                 {
-                    return CardEffectCommons.CanTriggerOnTrashHand(hashtable, null, cardSource => cardSource.Owner == card.Owner) && 
-                        (card.PermanentOfThisCard().TopCard.EqualsTraits("Demon") || 
-                            card.PermanentOfThisCard().TopCard.EqualsTraits("Titan"));
+                    return CardEffectCommons.IsExistOnBattleAreaDigimon(card)
+                        && CardEffectCommons.CanTriggerOnTrashHand(hashtable, null, cardSource => cardSource.Owner == card.Owner)
+                        && CardEffectCommons.IsOwnerTurn(card);
                 }
 
                 bool CanActivateCondition(Hashtable hashtable)
                 {
-                    return CardEffectCommons.IsExistOnBattleArea(card) &&
-                        CardEffectCommons.IsOwnerTurn(card) &&
-                        CardEffectCommons.HasMatchConditionOwnersCardInTrash(card, CanSelectCardCondition);
+                    return CardEffectCommons.IsExistOnBattleAreaDigimon(card) 
+                        && CardEffectCommons.HasMatchConditionOwnersCardInTrash(card, CanSelectCardCondition)
+                        && (card.PermanentOfThisCard().TopCard.EqualsTraits("Demon") 
+                            || card.PermanentOfThisCard().TopCard.EqualsTraits("Titan"));
                 }
 
                 IEnumerator ActivateCoroutine(Hashtable _hashtable)
