03255aec7557eaf58043220c1f543676f117f65e
x89rt2 <x89rt2@gmail.com>
2026-01-09 11:26:51 -0500

    Update BT15_054.cs
    Update cardname conditions

--- CardEffect/BT15/Green/BT15_054.cs
+++ CardEffect/BT15/Green/BT15_054.cs
@@ -16,7 +16,7 @@ namespace DCGO.CardEffects.BT15
             {
                 bool PermanentCondition(Permanent targetPermanent)
                 {
-                    return targetPermanent.TopCard.CardNames.Contains("Rosemon");
+                    return targetPermanent.TopCard.EqualsCardName("Rosemon");
                 }
 
                 cardEffects.Add(CardEffectFactory.AddSelfDigivolutionRequirementStaticEffect(
@@ -253,7 +253,7 @@ namespace DCGO.CardEffects.BT15
                 {
                     if (CardEffectCommons.IsExistOnBattleArea(card))
                     {
-                        if (card.PermanentOfThisCard().DigivolutionCards.Count((cardSource) => cardSource.CardNames.Contains("Rosemon") || cardSource.CardNames.Contains("X Antibody") || cardSource.CardNames.Contains("XAntibody")) >= 1)
+                        if (card.PermanentOfThisCard().DigivolutionCards.Count((cardSource) => cardSource.EqualsCardName("Rosemon") || cardSource.EqualsCardName("X Antibody")) >= 1)
                         {
                             return true;
                         }
