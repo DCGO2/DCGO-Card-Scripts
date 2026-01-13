b49ed3c6fa112a957a4416d98912cfc373ec6728
x89rt2 <x89rt2@gmail.com>
2026-01-13 09:52:16 -0500

    Update LM_012.cs

--- CardEffect/LM/Green/LM_012.cs
+++ CardEffect/LM/Green/LM_012.cs
@@ -219,7 +219,7 @@ namespace DCGO.CardEffects.LM
 
                 string EffectDiscription()
                 {
-                    return "";
+                    return "[All Turns] [Once Per Turn] When one of your Digimon with [Angoramon] in their texts deletes an opponent's Digimon in battle, trash the top card of their security stack.";
                 }
 
                 bool CanUseCondition(Hashtable hashtable)
