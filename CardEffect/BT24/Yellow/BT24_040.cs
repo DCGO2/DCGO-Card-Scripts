1b833ce698606f87dcb874d667ee7fb27cac0b5f
x89rt2 <x89rt2@gmail.com>
2026-01-16 18:40:02 -0500

    Update BT24_040.cs

--- CardEffect/BT24/Yellow/BT24_040.cs
+++ CardEffect/BT24/Yellow/BT24_040.cs
@@ -405,7 +405,7 @@ namespace DCGO.CardEffects.BT24
                                     toTop: false).PutSecurity()
                                 );
     
-                                if (card.Owner.SecurityCards.Contains(topCard) || card.Owner.Enemy.SecurityCards.Contains(topCard) || topCard.IsToken)
+                                if (card.Owner.SecurityCards.Contains(topCard) || card.Owner.Enemy.SecurityCards.Contains(topCard) || (securityCard.IsToken && !CardEffectCommons.IsExistOnBattleArea(securityCard)))
                                 {
                                     foreach (Permanent permanent in removedPermanents)
                                     {
