1b425aefc29307cd43c4e01c49239c14a7f5d017
x89rt2 <x89rt2@gmail.com>
2026-01-22 15:37:13 -0500

    Create EX11_003.cs
    Not sure how to check if added card to security is !cardSource.IsFlipped

---
+++ CardEffect/EX11/Green/EX11_003.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+
+// Puroromon
+namespace DCGO.CardEffects.EX11
+{
+    public class EX11_003 : CEntity_Effect
+    {
+        public override List<ICardEffect> CardEffects(EffectTiming timing, CardSource card)
+        {
+            List<ICardEffect> cardEffects = new List<ICardEffect>();
+
+            #region Inherited
+
+            if (timing == EffectTiming.OnAddSecurity)
+            {
+                ActivateClass activateClass = new ActivateClass();
+                activateClass.SetUpICardEffect("Draw 1.", CanUseCondition, card);
+                activateClass.SetUpActivateClass(CanActivateCondition, ActivateCoroutine, -1, true, EffectDiscription());
+                cardEffects.Add(activateClass);
+
+                string EffectDiscription()
+                {
+                    return "[Your Turn] [Once Per Turn] When face-up [Royal Base] trait cards are placed in your security stack, <Draw 1>.";
+                }
+
+                bool CanUseCondition(Hashtable hashtable)
+                {
+                    return CardEffectCommons.IsExistOnBattleArea(card)
+                        && CardEffectCommons.IsOwnerTurn(card)
+                        && CardEffectCommons.CanTriggerWhenAddSecurity(hashtable, player => player == card.Owner);
+                }
+
+                bool CanActivateCondition(Hashtable hashtable)
+                {
+                    return CardEffectCommons.IsExistOnBattleArea(card)
+                        && card.Owner.LibraryCards.Count >= 1;
+                }
+
+                IEnumerator ActivateCoroutine(Hashtable _hashtable)
+                {
+                    yield return ContinuousController.instance.StartCoroutine(new DrawClass(
+                        player: card.Owner,
+                        drawCount: 1,
+                        cardEffect: activateClass).Draw()
+                    );
+                }
+            }
+
+            #endregion
+
+            return cardEffects;
+        }
+    }
+}
