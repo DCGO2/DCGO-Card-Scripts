bad7670e734cb16776bd9355269ab5258c40504a
x89rt2 <x89rt2@gmail.com>
2026-02-01 17:43:31 -0500

    Apply suggestions from code review
    Co-authored-by: hooperk <hooperk@tcd.ie>

--- CardEffect/EX11/Green/EX11_034.cs
+++ CardEffect/EX11/Green/EX11_034.cs
@@ -354,6 +354,7 @@ namespace DCGO.CardEffects.EX11
 
             bool SharedCanActivateCondition1(Hashtable hashtable, ActivateClass activateClass)
             {
+                activateClass.SetEffectName(SharedEffectName1(card.Owner.SecurityCards.Count(cardSource => !cardSource.IsFlipped)));
                 return CardEffectCommons.IsExistOnBattleArea(card)
                     && CardEffectCommons.HasMatchConditionOwnersHand(card, cardSource => CanSelectCardCondition1(cardSource, activateClass));
             }
