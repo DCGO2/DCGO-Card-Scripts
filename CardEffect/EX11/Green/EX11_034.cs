6e8ab8f8b3747a2bba31ee84713f0c797cb82657
x89rt2 <x89rt2@gmail.com>
2026-02-01 14:09:19 -0500

    Fix coroutine for playing selected card

--- CardEffect/EX11/Green/EX11_034.cs
+++ CardEffect/EX11/Green/EX11_034.cs
@@ -170,6 +170,7 @@ namespace DCGO.CardEffects.EX11
                         selectCardEffect.SetUpCustomMessage("Select 1 card to place.", "The opponent is selecting 1 card to place.");
 
                         yield return ContinuousController.instance.StartCoroutine(selectCardEffect.Activate());
+                        
                         if (selectedCard != null) yield return ContinuousController.instance.StartCoroutine(
                             CardEffectCommons.PlayPermanentCards(new List<CardSource>() { selectedCard }, activateClass, false, false, SelectCardEffect.Root.Trash, true));
                     }
