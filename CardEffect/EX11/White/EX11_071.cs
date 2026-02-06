d20d3c1f7fadeca07c360c4d8ba84998e85a344b
x89rt2 <x89rt2@gmail.com>
2026-02-06 16:06:56 -0500

    Update EX11_071.cs

--- CardEffect/EX11/White/EX11_071.cs
+++ CardEffect/EX11/White/EX11_071.cs
@@ -102,7 +102,7 @@ namespace DCGO.CardEffects.EX11
                     return (cardSource.EqualsTraits("Royal Knight")
                         || cardSource.EqualsTraits("Liberator"))
                         && cardSource.GetCostItself >=4
-                        && CardEffectCommons.CanPlayAsNewPermanent(cardSource: cardSource, payCost: true, cardEffect: activateClass, fixedCost: cardSource.GetCostItself-4);
+                        && CardEffectCommons.CanPlayAsNewPermanent(cardSource: cardSource, payCost: true, cardEffect: activateClass, fixedCost: cardSource.GetCostItself-2);
                 }
 
                 IEnumerator ActivateCoroutine(Hashtable hashtable)
